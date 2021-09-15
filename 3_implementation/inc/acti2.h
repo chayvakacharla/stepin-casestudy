808601bf272ffc8c64f582dab7905c02698df4b1
chayvakacharla <89779094+chayvakacharla@users.noreply.github.com>
2021-09-15 21:30:50 +0530

    Update acti2.h

--- 3_implementation/inc/acti2.h
+++ 3_implementation/inc/acti2.h
@@ -12,7 +12,7 @@ void Init_ADC();
 /**
  * @brief A function to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
  * 
- * @param ch 
+ * @ ch 
  * @return uint16_t 
  */
 uint16_t Read_ADC(uint8_t ch);
