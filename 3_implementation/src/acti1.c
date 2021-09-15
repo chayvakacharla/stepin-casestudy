f705de73a17841b5e2e6df472db075e54bf0c650
chayvakacharla <89779094+chayvakacharla@users.noreply.github.com>
2021-09-15 21:16:49 +0530

    Update acti1.c

--- 3_implementation/src/acti1.c
+++ 3_implementation/src/acti1.c
@@ -1,13 +1,4 @@
-/**
- * @file act1.c
- * @author Tejaswi devina (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include "act1.h"
 
 /**
